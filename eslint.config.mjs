57458ae020006cc117de45f1de3e3c2871297213
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2026-05-26 14:52:46 +0200

    add new eslint plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import reactRefresh from "eslint-plugin-react-refresh";
+import reactPerf from "eslint-plugin-react-perf";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "react-refresh": reactRefresh,
+      "react-perf": reactPerf,
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+      "react-perf/jsx-no-new-object-as-prop": "warn",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
