63d4a704cdff571e381c156d7199d5eeb89b8e79
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2026-05-27 13:57:40 +0200

    fix: enhance test coverage configuration and reporting

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,18 @@ export default defineConfig({
     environment: "node",
     globals: true,
     include: ["src/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.ts", "!src/**/*.tsx"],
+      exclude: ["src/**/*.d.ts"],
+      reporter: [["lcovonly"], ["html"], ["cobertura"], ["text-summary"]],
+      thresholds: {
+        statements: 30,
+        branches: 25,
+        functions: 20,
+        lines: 30,
+      },
+    },
   },
   resolve: {
     alias: {
