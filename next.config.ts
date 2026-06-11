c21c49667ac568e50d3366dc1aef090001baa652
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2026-06-11 16:56:19 +0200

    feat: enhance environment variable logging and add missing keys

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  logging: {
+    browserToTerminal: true,
+  },
 };
 
 export default nextConfig;
