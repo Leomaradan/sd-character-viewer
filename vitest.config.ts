66a3bce6aa598064d24afb85118401d1104977ac
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2026-09-16 13:55:16 +0200

    fix: update branch coverage threshold in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       thresholds: {
         autoUpdate: (newThreshold) => Math.floor(newThreshold),
         statements: 96,
-        branches: 87,
+        branches: 88,
         functions: 98,
         lines: 96,
       },
